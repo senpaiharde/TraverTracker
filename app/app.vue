7187bf0ccb3b4c9b005f26b534cd7c9c92460205
senpaiharde <xxslavan1@gmail.com>
2025-07-17 20:40:26 +0300

    startup

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,47 @@
+<script>
+import SimpleCounter from './cmps/SimpleCounter.vue'
+
+export default {
+    components: {
+        SimpleCounter,
+    }
+}
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <h1>Hello Vue</h1>
+    <SimpleCounter :start-from="9"/>
+    <SimpleCounter :start-from="2"/>
+    <SimpleCounter />
+
+    <img src="/boy.png" alt="">
 </template>
+
+<style scoped>
+header {
+	line-height: 1.5;
+}
+
+.logo {
+	display: block;
+	margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+	header {
+		display: flex;
+		place-items: center;
+		padding-right: calc(var(--section-gap) / 2);
+	}
+
+	.logo {
+		margin: 0 2rem 0 0;
+	}
+
+	header .wrapper {
+		display: flex;
+		place-items: flex-start;
+		flex-wrap: wrap;
+	}
+}
+</style>
